592bf72e4fcf8350811e902ad1edab4eadf31870
andreidumitrescu2405 <79709164+andreidumitrescu2405@users.noreply.github.com>
2022-03-23 16:22:52 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,191 @@
+import pygame
+import time
+from pygame.locals import *
+import random
+
+SIZE = 40
+BACKGROUND_COLOR = (110, 110, 5)
+
+# class WallTerrain:
+#     def __init__(self, parent_screen):
+#         self.terrain = pygame.image.load("resources/wallterrain.jpg").convert()
+#         self.parent_screen = parent_screen
+#         for self.y in range(0, 800):
+#             self.x = 0
+#             self.parent_screen.blit(self.terrain, (self.x, self.y))
+#             pygame.display.flip()
+
+#     def draw(self):
+#         self.parent_screen.blit(self.terrain, (self.x, self.y))
+#         pygame.display.flip()
+
+class Apple:
+    def __init__(self, parent_screen):
+        self.image = pygame.image.load("resources/apple.jpg").convert()
+        self.parent_screen = parent_screen
+        self.x = SIZE*3
+        self.y = SIZE*3
+    def draw(self):
+        self.parent_screen.blit(self.image, (self.x, self.y))
+        pygame.display.flip()
+    
+    def move(self):
+        self.x = random.randint(0,24)*SIZE
+        self.y = random.randint(0,19)*SIZE
+
+class Snake:
+    def __init__(self, parent_screen, length):
+        self.length = length
+        self.parent_screen = parent_screen
+        self.block = pygame.image.load("resources/block.jpg").convert()
+        self.x = [SIZE]*length
+        self.y = [SIZE]*length
+        self.direction = 'down'
+
+    def draw(self):
+        self.parent_screen.fill((110, 110, 5))
+        for i in range(self.length):
+            self.parent_screen.blit(self.block, (self.x[i], self.y[i]))
+        pygame.display.flip()
+
+    def increase_length(self):
+        self.length += 1
+        self.x.append(-1)
+        self.y.append(-1)
+    
+    def move_left(self):
+        if self.direction != 'right':
+            self.direction = 'left'
+
+    def move_right(self):
+        if self.direction != 'left':
+            self.direction = 'right'
+
+    def move_up(self):
+        if self.direction != 'down':
+            self.direction = 'up'
+
+    def move_down(self):
+        if self.direction != 'up':
+            self.direction = 'down'
+
+    def walk(self):
+        for i in range(self.length-1,0,-1):
+            self.x[i] = self.x[i-1]
+            self.y[i] = self.y[i-1]
+        if self.direction == 'up':
+            self.y[0] -= SIZE
+        if self.direction == 'down':
+            self.y[0] += SIZE
+        if self.direction == 'left':
+            self.x[0] -= SIZE
+        if self.direction == 'right':
+            self.x[0] += SIZE
+        self.draw()
+        
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.surface = pygame.display.set_mode((1000, 800))
+        self.surface.fill((BACKGROUND_COLOR))
+        self.snake = Snake(self.surface, 1)
+        self.snake.draw()
+        self.apple = Apple(self.surface)
+        self.apple.draw()
+
+    def is_collision(self, x1, y1, x2, y2):
+        # if x1 >= x2 and x1 <= x2 + SIZE:
+        #     if y1 >= y2 and y1 <= y2 + SIZE:
+        #         return True
+        # return False
+        if x1 == x2 and y1 == y2:
+            return True
+        return False
+
+    def play(self):
+        self.snake.walk()
+        self.apple.draw()
+        self.display_score()
+        pygame.display.flip()
+
+        # snake coliding with apple
+        if self.is_collision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
+            self.snake.increase_length()
+            self.apple.move()
+        
+        for y_axis in range(0, 800):
+            x_axis = 0
+            if self.is_collision(self.snake.x[0], self.snake.y[0], x_axis, y_axis):
+                exit(0)
+        for y_axis in range(0, 800):
+            x_axis = 1000
+            if self.is_collision(self.snake.x[0], self.snake.y[0], x_axis, y_axis):
+                exit(0)
+        for x_axis in range(0, 1000):
+            y_axis = 800
+            if self.is_collision(self.snake.x[0], self.snake.y[0], x_axis, y_axis):
+                exit(0)
+        for x_axis in range(0, 1000):
+            y_axis = 0
+            if self.is_collision(self.snake.x[0], self.snake.y[0], x_axis, y_axis):
+                exit(0)
+        # snake coliding with itself
+        for i in range(1, self.snake.length):
+            if self.is_collision(self.snake.x[0], self.snake.y[0], self.snake.x[i], self.snake.y[i]):
+                raise "Game over"
+
+    def show_game_over(self):
+        self.surface.fill()
+
+    def display_score(self):
+        font = pygame.font.SysFont('arial', 30)
+        score = font.render(f'Score: {self.snake.length}', True, (200, 200, 200))
+        self.surface.blit(score, (800,10))
+
+    def run(self):
+        running = True
+
+        while running:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        running = False
+
+                    if event.key == K_UP:
+                        self.snake.move_up()
+
+                    if event.key == K_DOWN:
+                        self.snake.move_down()
+
+                    if event.key == K_LEFT:
+                        self.snake.move_left()
+
+                    if event.key == K_RIGHT:
+                        self.snake.move_right()
+
+                elif event.type == QUIT:
+                    running = False
+            try:
+                self.play()
+            except Exception as e:
+                self.show_game_over()
+
+            time.sleep(.1)
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
+
+
+    
+
+'''
+Python Game Tutorial:
+https://www.youtube.com/watch?v=8dfePlONtls&ab_channel=freeCodeCamp.org
+1:13:23
+
+Pytorch for DeepLearning
+https://www.youtube.com/watch?v=GIsg-ZUy0MY&ab_channel=freeCodeCamp.org
+21:16
+
+'''
